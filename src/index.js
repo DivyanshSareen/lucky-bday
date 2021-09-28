ce212735aaf355a3dfdc0abf35cf4f56bdcb98e8
DivyanshSareen <59335572+DivyanshSareen@users.noreply.github.com>
2021-09-28 15:36:12 +0530

    added calcSum()

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ var res = document.getElementById("result");
 
 enter.addEventListener("click", handleEnter);
 
+function calcSum(s) {
+  let num = parseInt(s, 10);
+  let total = 0;
+  while (num > 0) {
+    total += Math.floor(num % 10);
+    num = Math.floor(num / 10);
+  }
+  return total;
+}
 function handleEnter() {
-  console.log(bdate.value);
-  var date = bdate.value.split("-").map((e) => parseInt(e, 10));
+  // console.log(bdate.value);
+  var date = bdate.value.split("-").map((e) => calcSum(e));
+  // console.log(date);
   var t = date.reduce((total, num) => (total += num));
   var n = lnum.value;
+  // console.log(t);
   let isLucky = t % n === 0 ? true : false;
   if (isLucky) {
     res.innerHTML = "Birthday is Lucky!✌️";
